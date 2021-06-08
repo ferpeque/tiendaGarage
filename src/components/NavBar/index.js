c5893799bc3e062129571bc211587412877559c1
fer peque <fernandopequeno@gmail.com>
2021-06-08 20:03:50 -0300

    img

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,23 +4,23 @@ export const NavBar = () => {
 return (
 <nav className="navbar navbar-expand-lg navbar-light container ">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <img src="/images/logo1.svg" className="logo" />
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
+    <div className="collapse navbar-collapse barra" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
           <a className="nav-link" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Productos</a>
+          <a className="nav-link" >Productos</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Nosotros</a>
+          <a className="nav-link" >Nosotros</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Carrito</a>
+          <a className="nav-link" >Carrito</a>
         </li>
       </ul>
     </div>
