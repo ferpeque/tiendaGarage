c94c3df73ef9df0cc5cb39af292585f00eb2fcc2
fer peque <fernandopequeno@gmail.com>
2021-06-13 22:28:38 -0300

    1.3

--- src/components/CartWidget/index.js
+++ src/components/CartWidget/index.js
@@ -2,10 +2,8 @@ import './style.css';
 
 export const IconCarrito = () => {
     return (
-        <button 
-        ><i className="fas fa-shopping-cart boton-carrito"></i>
-        <span
-        id="contadorCarrito">0</span>
+        <button ><i className="fas fa-shopping-cart boton-carrito"></i>
+    
         </button>
         
     )
