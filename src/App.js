17650eae4550e2e3d8312b35ff5f195320bb80c0
fer peque <fernandopequeno@gmail.com>
2021-06-20 10:45:36 -0300

    1.2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { IconCarrito } from "./components/CartWidget";
 import { HomeContainer } from "./Container/HomeContainer";
+import React, {useEffect, useState} from "react"
+
 
 
 function App() {
 
-  async function getData () {
-    const response = await fetch("https://mocki.io/v1/068fb553-24a4-440f-ba3d-dc57f1cc13cd")
-    const data = await response.json();
-    return data
-}
+  const [productos, setProductos] = useState ([]);
 
-console.log(getData())
-
+    useEffect ( async () => {
+      const response = await fetch("https://mocki.io/v1/068fb553-24a4-440f-ba3d-dc57f1cc13cd")
+      const json = await response.json()  
+      setProductos(json)
+      console.log(productos)
+    }, [])
 
   return (     
         <HomeContainer/>    
